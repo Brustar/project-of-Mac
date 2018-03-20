8b3692aabe2099a4f7da2d27b23bbcbc76f06a04
Brustar.xiao <yeals2@hotmail.com>
2018-03-20 09:10:58 +0800

    Initial Commit

---
+++ CIC library/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CIC library
+//
+//  Created by Brustar.xiao on 2018/3/20.
+//  Copyright © 2018年 Brustar.xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CIC library/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CIC library
+//
+//  Created by Brustar.xiao on 2018/3/20.
+//  Copyright © 2018年 Brustar.xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
